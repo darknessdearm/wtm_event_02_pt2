f62bf3a1c00f322fc236f72594c7ae34b2099ccf
darknessdearm <mei.n.s.1998@gmail.com>
2026-05-13 22:59:35 +0700

    fix lint

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
-import CharacterForm from "./CharacterForm";
 import CharacterHistory from "./CharacterHistory";
 import ItemCollection from "./ItemCollection";
 import RewardCard from "./RewardCard";
 import Navbar from "./Navbar";
 
-export { CharacterForm, CharacterHistory, ItemCollection, RewardCard, Navbar };
+export { CharacterHistory, ItemCollection, RewardCard, Navbar };
